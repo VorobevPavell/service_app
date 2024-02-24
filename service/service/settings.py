d8c5ad7989568de3f48d441e64dfa96816410ff6
vorobevpasha <korochepashtet@yandex.ru>
2024-02-24 21:14:14 +0300

    day 5 - 3 project is done!

--- service/service/settings.py
+++ service/service/settings.py
@@ -16,7 +16,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party
     'rest_framework',
+    'cachalot',
 
     # Local
     'clients',
